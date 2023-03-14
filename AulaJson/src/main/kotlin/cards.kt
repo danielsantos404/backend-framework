398db090f4e080e4b55e66d2487bf08cf79c46c8
danielsantos404 <dan.danielfs@gmail.com>
2023-03-14 02:46:44 -0300

    Atividade Json publicada

---
+++ AulaJson/src/main/kotlin/cards.kt
@@ -0,0 +1,22 @@
+class Cards(
+
+        val name: String,
+        val names: List<String>,
+        val manaCost: String,
+        val cmc: Int,
+        val colors: List<String>,
+        val colorIdentity: List<String>,
+        val type: String,
+        val supertypes: List<String>,
+        val types: List<String>,
+        val subtypes: List<String>,
+        val rarity: String,
+        val Set: String,
+        val text: String,
+        val artist: String,
+        val number: String,
+        val power: String,
+        val toughness: String,
+        val layout: String,
+        val multiverseid: String
+)
---
+++ AulaJson/src/main/kotlin/sets.kt
@@ -0,0 +1,13 @@
+class Sets(
+
+    val code: String,
+    val name: String,
+    val type: String,
+    val border: String,
+    val mkm_id: Int,
+    val booster: List<String>,
+    val mkm_name: String,
+    val releaseDate: String,
+    val magicCardsInfoCode: String,
+    val block: String
+)
